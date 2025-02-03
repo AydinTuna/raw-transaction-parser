a8f35633dd85ecd73820b77f0b3a93651240b8ae
AydinTuna <aydinbahadirtuna@gmail.com>
2025-02-03 21:19:55 +0300

    quick fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Bitcoin and Ethereum Parser
+    <div className="flex flex-col items-center justify-center sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 mb-4 text-center">
+        Bitcoin & Ethereum Parser
+      </h1>
+      <p className="text-lg text-gray-600 text-center max-w-xl">
+        Decode and visualize raw Bitcoin and Ethereum transactions.
+      </p>
     </div>
   );
 }
