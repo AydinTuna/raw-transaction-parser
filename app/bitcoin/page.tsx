a67e4ed8fcf4e4f5878cc1c9360376a8f52dd316
benbaho <aydinbahadirtuna@gmail.com>
2025-02-04 12:31:53 +0300

    project build

--- app/bitcoin/page.tsx
+++ app/bitcoin/page.tsx
@@ -74,7 +74,6 @@ export default function Page() {
                 </button>
             </form>
 
-
             <div className="w-full max-w-2xl mt-8">
                 <p className="font-bold w-full text-start mb-2 text-gray-700">Raw Transaction Data:</p>
                 {loading && <p className="text-black mt-4 font-bold w-full text-center">Loading...</p>}
